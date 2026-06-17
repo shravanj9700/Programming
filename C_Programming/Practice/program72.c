0621c6b0321b3baa7f3926192862df129779ce35
shravanj9700 <shravanj9700@gmail.com>
2026-06-17 10:24:40 +0530

    Add files via upload

---
+++ C_Programming/Practice/program72.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+
+    iCnt = 1;
+
+    do
+    {
+        printf("Jay Ganesh...\n");
+        iCnt++;
+    }while(iCnt <= 5);  // semi colon imp for do-while loop
+
+    return 0;
+}
---
+++ C_Programming/Practice/program73.c
@@ -0,0 +1,21 @@
+// Input : 5
+// output : 1 2 3 4 5
+
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+
+    iCnt = 1;
+
+    while(iCnt <= 5)
+    {
+        printf("%d\t",iCnt);
+        iCnt++;
+    }
+
+    printf("\n");
+
+    return 0;
+}
