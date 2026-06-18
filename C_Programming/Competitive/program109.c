37915d05f2facfc639f8cc28953ae6683cf80ec7
shravanj9700 <shravanj9700@gmail.com>
2026-06-18 09:24:40 +0530

    Add files via upload

---
+++ C_Programming/Competitive/program109.c
@@ -0,0 +1,55 @@
+/*
+    Input : iRow = 6        iCol = 6
+
+    Output :        %   %   %   %   %   %
+                    %   %               %
+                    %      %            %
+                    %           %       %
+                    %               %   %
+                    %   %   %   %   %   %
+*/
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    if(iRow != iCol)
+    {
+        printf("Invalid Input\n");
+        printf("Number of rows and columns should be same \n");
+        return;
+    }
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i == j || i == 1 || j == 1 || i == iRow || j == iCol)
+            {
+                printf("%%\t");
+            }
+            else
+            {
+                printf(" \t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows : \n");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns : \n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
---
+++ C_Programming/Competitive/program98.c
@@ -0,0 +1,48 @@
+/*
+    Input : iRow = 5        iCol = 5
+
+    Output :    a   b   c   d   e
+                1   2   3   4   5
+                a   b   c   d   e
+                1   2   3   4   5
+                a   b   c   d   e
+*/
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+    char ch = '\0';
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1, ch = 'a'; j <= iCol; j++, ch++)
+        {
+            if(i % 2 == 0)
+            {
+                printf("%d\t",j);
+            }
+            else
+            {
+                printf("%c\t",ch);                
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows : \n");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns : \n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
