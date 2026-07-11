d90ca7d8bdfba505e94f25e6c44d10b9ba8c892c
shravanj9700 <shravanj9700@gmail.com>
2026-07-11 13:10:41 +0530

    Add files via upload

---
+++ C_Programming/Practice/program247.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void strlenX(char *str)
+{
+    *str = 'A';    
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+    int iRet = 0;
+
+    printf("Enter string : \n");
+    scanf("%[^'\n]s",Arr);
+
+    strlenX(Arr);
+
+    printf("String is : %s\n",Arr);
+
+    return 0;
+}
+
+// It is constant character 
+// output will be starting with A is constant
---
+++ C_Programming/Practice/program259.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void CountUpdate(char *str)
+{
+    while (*str != '\0')
+    {
+        if(*str == 'l')
+        {
+            *str = 'L';   
+        }
+        str++;        
+    }  
+}
+
+int main()
+{
+    char Arr[50] = {'\0'};
+
+    printf("Enter string : \n");
+    scanf("%[^'\n]s",Arr);
+
+    CountUpdate(Arr);
+
+    printf("Updated String is : %s\n",Arr);
+
+    return 0;
+}
