6875f283ef2054787fbb3256b9168181c93293f9
shravanj9700 <shravanj9700@gmail.com>
2026-07-14 09:15:00 +0530

    Add files via upload

---
+++ C_Programming/Practice/program301.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int No = 97;
+
+    printf("Decimal : %d\n",No);
+    printf("Octal : %o\n",No);
+    printf("HexaDecimal : %x\n",No);
+
+
+    return 0;
+} 
