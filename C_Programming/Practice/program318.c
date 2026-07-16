5818b5b3e047773485284d3b79dc7e4379e74bc6
shravanj9700 <shravanj9700@gmail.com>
2026-07-16 17:24:21 +0530

    Add files via upload

---
+++ C_Programming/Practice/program318.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+typedef unsigned int UINT;
+
+int main()
+{
+    UINT iMask = 0x1;
+    int iCnt = 0;
+
+    printf("%d\n",iMask);       // 1
+    iMask = iMask << 1;
+
+    printf("%d\n",iMask);       // 2
+    iMask = iMask << 1;
+    
+    printf("%d\n",iMask);       // 4
+    iMask = iMask << 1;
+
+    printf("%d\n",iMask);       // 8
+    iMask = iMask << 1;
+
+    printf("%d\n",iMask);       // 16
+    iMask = iMask << 1;
+
+    printf("%d\n",iMask);       // 32
+    iMask = iMask << 1;
+
+    printf("%d\n",iMask);       // 64
+    iMask = iMask << 1;
+
+    printf("%d\n",iMask);       // 128
+    iMask = iMask << 1;
+
+    printf("%d\n",iMask);       // 256
+    iMask = iMask << 1;
+    
+
+    return 0;
+}
