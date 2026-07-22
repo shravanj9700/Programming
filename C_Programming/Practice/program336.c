3acf52ba8da4aa87f347c12b1bf28f81a88821c2
shravanj9700 <shravanj9700@gmail.com>
2026-07-23 00:27:00 +0530

    Add files via upload

---
+++ C_Programming/Practice/program336.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+typedef unsigned int UINT;
+
+// POSITION : 4 
+// Hardcoded value
+
+UINT OffBit(UINT iNo, UINT iPos)
+{
+    UINT iMask = 0xFFFFFFF7;
+    UINT iResult = 0;
+
+    // FILTER
+    if(iPos < 1 || iPos > 32)
+    {
+        printf("Invalid BIT position\n");
+        return iNo;
+    }
+
+    iMask = iMask << (iPos - 1);
+
+    iResult = iNo ^ iMask;
+
+    return iResult;
+}
+
+int main()
+{
+    UINT iValue = 0;
+    UINT iRet = 0;
+    UINT iLocation = 0;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    printf("Enter the BIT position : \n");
+    scanf("%d",&iLocation);
+
+    iRet = OffBit(iValue, iLocation);
+
+    printf("Updated number is : %d\n",iRet);
+
+    return 0;
+}
