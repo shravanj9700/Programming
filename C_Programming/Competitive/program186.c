fbd3b919cb05878f4c91bebdc544a093c5c7eb82
shravanj9700 <shravanj9700@gmail.com>
2026-07-28 20:43:14 +0530

    Add files via upload

---
+++ C_Programming/Competitive/program186.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<string.h>
+#include<unistd.h>
+#include<io.h>
+
+#define BUFFER_SIZE 1024
+
+int CountSmall(char FileName[])
+{
+    char Buffer[BUFFER_SIZE] = {'\0'};
+    int fd = 0, iRet = 0, iCount = 0, i = 0;
+
+    fd = open("Demo.txt",O_RDONLY);
+
+    if(fd == -1)
+    {
+        printf("Unable to open the file\n");
+        return -1;
+    }
+
+    while((iRet = read(fd,Buffer,sizeof(Buffer))) != 0)
+    {
+        for(i = 0; i < iRet; i++)
+        {
+            if(Buffer[i] >= 'a' && Buffer[i] <= 'z')
+            {
+                iCount++;
+            }
+        }
+        memset(Buffer,'\0',sizeof(Buffer));
+    }
+    return iCount;
+}
+
+int main()
+{
+    char  FileName[30];
+    int iRet = 0;
+
+    printf("Enter file name : \n");
+    scanf("%[^'\n']s",FileName);
+
+    iRet = CountSmall(FileName);
+    
+    printf("Number of small characters are : %d\n",iRet);
+
+    return 0;
+}
---
+++ C_Programming/Competitive/program187.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<string.h>
+#include<io.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+#define BUFFER_SIZE 1024
+
+int CountCapital(char FileName[])
+{
+    char Buffer[BUFFER_SIZE] = {'\0'};
+    int fd = 0, iRet = 0, iCount = 0, i = 0;
+
+    fd = open(FileName,O_RDONLY);
+
+    if(fd == -1)
+    {
+        printf("Unable to open file\n");
+        return -1;
+    }
+
+    while((iRet = read(fd,Buffer,sizeof(Buffer))) != 0)
+    {
+        for(i = 0; i < iRet; i++)
+        {
+            if(Buffer[i] >= 'A' && Buffer[i] <= 'Z')
+            {
+                iCount++;
+            }
+        }
+        memset(Buffer,'\0',sizeof(Buffer));
+    }
+    return iCount;
+}
+
+int main()
+{
+    char FName[30];
+    int iRet = 0;
+
+    printf("Enter file name : \n");
+    scanf("%[^'\n']s",FName);
+
+    iRet = CountCapital(FName);
+
+    printf("Number of capital characters are : %d\n",iRet);
+
+    return 0;
+}
