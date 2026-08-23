555616a7ecf546bba435ebb2fed9079690c9ce8d
shravanj9700 <shravanj9700@gmail.com>
2026-08-23 18:35:24 +0530

    Add files via upload

---
+++ C++_Programming/Practice/program476.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+float Maximum(float No1, float No2)
+{
+    float Ans;
+
+    if(No1 > No2)
+    {
+        Ans = No1;   
+    }
+    else
+    {
+        Ans = No2;
+    }
+
+    return Ans;
+}
+
+int main()
+{
+    cout<<Maximum(21.5f, 11.2f)<<"\n";
+
+    return 0;
+}
