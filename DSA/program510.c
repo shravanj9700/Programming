7d7957ece132e2927fe2dbd9aeab83429713ce95
shravanj9700 <shravanj9700@gmail.com>
2026-08-28 23:15:21 +0530

    Add files via upload

---
+++ DSA/program510.c
@@ -0,0 +1,23 @@
+// Finally Using Recurssion
+
+#include<stdio.h>
+
+void Display()
+{
+    static int i = 1;
+
+    if( i <= 4 )
+    {
+        printf("Jay Ganesh...\n");
+        i++;
+
+        Display();
+    } 
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
