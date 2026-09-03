fc1af0f6fcf4d1f077fc050efd2d3ad27dfd6e04
shravanj9700 <shravanj9700@gmail.com>
2026-09-03 22:48:51 +0530

    Add files via upload

---
+++ DSA/CPP/program571.cpp
@@ -0,0 +1,130 @@
+#include<iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node * lchild;
+    struct node * rchild;
+};
+
+class Searching
+{
+    private:
+        int *Arr;
+        int iSize;
+
+    public: 
+        Searching(int iNo);
+        ~Searching();
+
+        void Accept();
+        void Display();
+
+        bool LinearSearch(int iNo);
+        bool BidirectionalSearch(int iNo);
+};
+
+Searching :: Searching(int iNo)
+{
+    iSize = iNo;
+    Arr = new int[iSize];
+}
+
+Searching :: ~Searching()
+{
+    delete []Arr;
+}
+
+void Searching :: Accept()
+{
+    int i = 0;
+
+    cout<<"Enter the elements : \n";
+
+    for(i = 0; i < iSize; i++)
+    {
+        cin>>Arr[i];
+    }
+}
+
+void Searching :: Display()
+{
+    int i = 0;
+
+    cout<<"Elements of the array are : \n";
+
+    for(i = 0; i < iSize; i++)
+    {
+        cout<<Arr[i]<<"\n";
+    }
+}
+
+bool Searching :: LinearSearch(int iNo)
+{
+    bool bFlag = false;
+    int i = 0;
+
+    for(i = 0; i < iSize; i++)
+    {
+        if(Arr[i] == iNo)
+        {
+            bFlag = true;
+            break;
+        }
+    }
+
+    return bFlag;
+}
+
+bool Searching :: BidirectionalSearch(int iNo)
+{
+    int iStart = 0;
+    int iEnd = 0;
+    bool bFlag = false;
+
+    iStart = 0;
+    iEnd = iSize - 1;
+
+    while(iStart <= iEnd)
+    {
+        if(Arr[iStart] == iNo || Arr[iEnd] == iNo)
+        {
+            bFlag = true;
+            break;
+        }
+
+        iStart++;
+        iEnd--;
+    }
+
+    return bFlag;
+}
+
+int main()
+{
+    Searching sobj(5);  
+
+    sobj.Accept();
+    sobj.Display();
+
+    if(sobj.LinearSearch(30) == true)
+    {
+        cout<<"Element is present : \n";
+    }
+    else
+    {
+        cout<<"There is no such element : \n";
+    }
+
+    if(sobj.BidirectionalSearch(30) == true)
+    {
+        cout<<"Element is present : \n";
+    }
+    else
+    {
+        cout<<"There is no such element : \n";
+    }
+
+    return 0;
+}
